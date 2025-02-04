f7fb41aa3a848573a5abd6abb4ecedca288b212d
cleoivvy <claraojobo@gmail.com>
2025-02-04 16:13:30 +0100

    settings update

--- maths/settings.py
+++ maths/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'maths-p2ah.onrender.com,localhost,127.0.0.1').split(',')
 
 
 
